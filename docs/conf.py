6cafb46aff602e1448eb48dddcbce9dacf955212
emptymalei <emptymalei@qq.com>
2014-11-15 21:19:54 -0700

    switch to bootstrap theme with a global nav bar

--- docs/conf.py
+++ docs/conf.py
@@ -114,10 +114,14 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 if not on_rtd:  # only import and set the theme if we're building docs locally
 #    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
+#    html_theme = 'sphinx_rtd_theme'
+    html_theme = 'bootstrap'
 #    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
     html_theme_path = ["_themes", ]
 
+html_sidebars = {
+   '**': ['bootstrapsidebar.html',],
+}
 # otherwise, readthedocs.org uses their theme by default, so no need to specify it
 
 
